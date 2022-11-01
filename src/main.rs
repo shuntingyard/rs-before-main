c183ac231263cf5ef1bd3addd0498d16257fb1c3
Tobias Frei <shuntingyard@gmail.com>
2022-11-01 10:18:16 +0100

    Building with fn _start() in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #![no_std]
 #![no_main]
 
-extern crate libc;
-
 #[no_mangle]
 // `pub extern "C" fn main() -> isize {` and the syntax without "C" seem to be equivalent.
-pub extern fn main() -> isize {
-    0
+pub extern fn _start() -> ! {
+    loop {}
 }
 
 #[panic_handler]
