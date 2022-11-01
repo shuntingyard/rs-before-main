105d87ad9608a9c2f7daad93be01d2ae3f29e6eb
Tobias Frei <shuntingyard@gmail.com>
2022-11-01 03:41:57 +0100

    Size (no_std, no_main C) 14K

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    std::process::exit(0)
+#![no_std]
+#![no_main]
+
+extern crate libc;
+
+#[no_mangle]
+// `pub extern "C" fn main() -> isize {` and the syntax without "C" seem to be equivalent.
+pub extern fn main() -> isize {
+    0
+}
+
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
