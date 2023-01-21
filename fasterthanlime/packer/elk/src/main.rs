ee74b789c687e22335530562442128b8dc715527
Tobias Frei <shuntingyard@gmail.com>
2023-01-21 22:05:25 +0100

    Making packer - part-2: Running ... without exec

--- fasterthanlime/packer/elk/src/main.rs
+++ fasterthanlime/packer/elk/src/main.rs
@@ -4,12 +4,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input_path = env::args().nth(1).expect("usage: elk FILE");
     let input = fs::read(&input_path)?;
 
+    println!("Analyzing: {:?}", input_path);
+
     let file = match delf::File::parse_or_print_error(&input[..]) {
         Some(f) => f,
         None => std::process::exit(1),
     };
 
     println!("{:#?}", file);
+
+    println!("Executing: {:?}", input_path);
+
+    use std::process::Command;
+    let status = Command::new(input_path).status()?;
+    if !status.success() {
+        return Err("process did not exit successfully".into());
+    }
 
     Ok(())
 }
